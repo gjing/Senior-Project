fffcccae9d690d539772b110a5c541c12dcc2701
gjing <gjing@calpoly.edu>
2015-05-19 21:13:49 -0700

    Just need to add equations now

--- liveGPS/app/src/main/java/com/example/george/livegps/GPS.java
+++ liveGPS/app/src/main/java/com/example/george/livegps/GPS.java
@@ -23,13 +23,17 @@ public class GPS extends Activity {
 
     private long startTime = 0L;
     private long timeInMilliseconds;
-    private double alt;
-    private double longitude;
-    private double latitude;
+    private double ialt;
+    private double initalt;
+    private double ilongitude;
+    private double ilatitude;
+    private long itime;
     private TextView personText;
     private TextView locationText;
     private LocationManager locationManager;
     private LinkedList list;
+    private double erad = 6371000.0;
+    private double c = 300000000.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,33 +49,40 @@ public class GPS extends Activity {
     public class MyLocationListener implements LocationListener {
         TextView tv = (TextView) findViewById(R.id.quote);
         TextView time = (TextView) findViewById(R.id.person);
+        private double alt;
+        private long t;
+        private long elapsed = 0;
+        private boolean started = false;
 
         @Override
         public void onLocationChanged(Location loc) {
-            Log.d("tag", "Finding Altitude");
-            double alt = loc.getAltitude();
-            Log.d("tag", "Alt: " + String.valueOf(alt));
-            Log.d("tag", "Finding Velocity");
-            double v = loc.getSpeed();
-            Log.d("tag", "Speed: " + String.valueOf(v));
-            Log.d("tag", "Finding Latitude");
-            double lat = loc.getLatitude();
-            Log.d("tag", "Lat: " + String.valueOf(lat));
-            Log.d("tag", "Finding Longitude");
-            double lon = loc.getLongitude();
-            Log.d("tag", "Lon: " + String.valueOf(lon));
-            Log.d("tag", "Finding Time");
-            long t = loc.getTime();
-            Log.d("tag", "Time: " + String.valueOf(t));
-            String Text = "My current location is: " +
-                    "\nLatitude = " + lat +
-                    "\nLongitude = " + lon +
-                    "\nAltitude = " + alt +
-                    "\nVelocity = " + v;
-            String count = "Time: "+t;
-            // Display location
-            tv.setText(Text);
-            time.setText(count);
+            if (!started) {
+                ialt = loc.getAltitude();
+                initalt = loc.getAltitude();
+                ilatitude = loc.getLatitude();
+                ilongitude = loc.getLongitude();
+                itime = loc.getTime();
+                t = itime;
+                elapsed = itime;
+                started = true;
+            }
+            else {
+                alt = loc.getAltitude() - initalt;
+                double latitude = loc.getLatitude() - ilatitude;
+                ilatitude = ilatitude - latitude;
+                double longitude = loc.getLongitude() - ilongitude;
+                ilongitude = ilongitude - longitude;
+                double altitude = loc.getAltitude() - ialt;
+                ialt = ialt - altitude;
+                t = loc.getTime() - elapsed;
+                elapsed = loc.getTime() - itime;
+                time.setText("Time Elapsed: " + elapsed/1000);
+                double x = Math.abs(latitude * erad / 360);
+                double y = Math.abs(longitude*erad/360);
+                double z = Math.abs(altitude);
+                double v = Math.sqrt(x*x + y*y + z*z)/t;
+                tv.setText("Velocity: "+v + ", Redshift: " + alt);
+            }
         }
 
         @Override
